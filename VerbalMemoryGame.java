e36220a89d200ed38621080ee2d5bdf2c32d3add
Waffles <waffles3438@gmail.com>
2024-01-11 16:52:17 -0500

    Update VerbalMemoryGame.java

--- VerbalMemoryGame.java
+++ VerbalMemoryGame.java
@@ -10,7 +10,20 @@ import java.util.ArrayList;
 public class VerbalMemoryGame extends World
 {
     private static boolean startgame = false;
-    private String[] newWords = {"apple", "banana", "car", "dog", "elephant", "fish", "guitar", "house", "ice cream", "jacket", "kangaroo", "lemon", "moon", "nose", "orange", "pizza", "queen", "rainbow", "sun", "tree", "umbrella", "violin", "water", "xylophone", "yellow", "zebra", "book", "cat", "desk", "egg", "fire", "goose", "hat", "island", "jungle", "kite", "lion", "mango", "nest", "ocean", "pencil", "quilt", "river", "snake", "table", "unicorn", "volcano", "window", "yacht", "zeppelin", "atom", "butterfly", "cloud", "diamond", "feather", "giraffe", "helicopter", "internet", "jellyfish", "kayak", "leopard", "mushroom", "nutmeg", "ostrich", "peacock", "quicksilver", "rhinoceros", "sandwich", "telephone", "vampire", "waterfall", "yogurt", "zealous", "chicken", "word", "interesting", "why"};
+    private String[] newWords = {"apple", "banana", "car", "dog", "elephant", "fish", "guitar", "house", "ice cream"
+        , "jacket", "kangaroo", "lemon", "moon", "nose", "orange", "pizza", "queen", "rainbow", "sun", "tree", "umbrella"
+        , "violin", "water", "xylophone", "yellow", "zebra", "book", "cat", "desk", "egg", "fire", "goose", "hat", "island"
+        , "jungle", "kite", "lion", "mango", "nest", "ocean", "pencil", "quilt", "river", "snake", "table", "unicorn", "volcano"
+        , "window", "yacht", "zeppelin", "atom", "butterfly", "cloud", "diamond", "feather", "giraffe", "helicopter", "internet"
+        , "jellyfish", "kayak", "leopard", "mushroom", "nutmeg", "ostrich", "peacock", "quicksilver", "rhinoceros", "sandwich"
+        , "telephone", "vampire", "waterfall", "yogurt", "zealous", "chicken", "word", "interesting", "why", "among", "us", "waffle"
+        , "pancake", "mouse", "computer", "science", "bird", "emerald", "banned", "night", "day", "carrot", "cheese", "spider"
+        , "monitor", "banana", "lamp", "wire", "mousepad", "phone", "chemistry", "classroom", "elite", "yoga", "x-ray"
+        , "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday", "trigonometry", "identity", "math", "pie"
+        , "keyboard", "camera", "window", "laptop", "grade", "accounting", "engineering", "farmer", "farming", "auto", "who"
+        , "rice", "dog", "cat", "horse", "house", "chair", "battery", "contest", "badminton", "soccer", "team", "police", "fighter"
+        , "pasta", "tower", "plane", "world", "war", "two", "flight", "air", "lives", "string", "text", "static", "private"
+        , "void", "boolean", "arguments", "integer", "float", "fly"};
     private ArrayList<String> seenWords;
     private Start start;
     private SeenButton seenbutton;
@@ -23,6 +36,7 @@ public class VerbalMemoryGame extends World
     private Label scorecount;
     private int rng;
     private Label randomword;
+    private int test = 0;
     
     /**
      * Constructor for objects of class VerbalMemoryGame.
@@ -67,24 +81,31 @@ public class VerbalMemoryGame extends World
     public void act(){
         if(startgame == true){
             setup();
-            rng = Greenfoot.getRandomNumber(newWords.length);
-            randomword.setValue(newWords[rng]);
+            if(test == 0){
+                rng = Greenfoot.getRandomNumber(newWords.length);
+                randomword.setValue(newWords[rng]);
+                test = 1;
+            }
             if(Greenfoot.mouseClicked(newbutton) && !seenWords.contains(newWords[rng])){
                 score++;
                 scorecount.setValue("Score: " + score);
                 seenWords.add(newWords[rng]);
+                test = 0;
             }
             else if(Greenfoot.mouseClicked(newbutton) && seenWords.contains(newWords[rng])){
                 life--;
                 lives.setValue("Lives: "+ life);
+                test = 0;
             }
             else if(Greenfoot.mouseClicked(seenbutton) && seenWords.contains(newWords[rng])){
                 score++;
                 scorecount.setValue("Score: " + score);
+                test = 0;
             }
             else if(Greenfoot.mouseClicked(seenbutton) && !seenWords.contains(newWords[rng])){
                 life--;
                 lives.setValue("Lives: "+ life);
+                test = 0;
             }
             
             if(life == 0){
